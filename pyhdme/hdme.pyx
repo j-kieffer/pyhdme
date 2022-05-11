bb55fe41caf9050e0ac1cb9c8c67b3b451a4844a
Edgar Costa <edgarc@mit.edu>
2022-05-11 16:22:48 -0400

    some fixes

--- pyhdme/hdme.pyx
+++ pyhdme/hdme.pyx
@@ -7,8 +7,10 @@
 from cysignals.signals cimport sig_on, sig_off
 from memory_allocator cimport MemoryAllocator
 from sage.libs.flint.types cimport fmpq
+from sage.libs.flint.fmpq cimport fmpq_get_mpq, fmpq_set_mpq
 from sage.rings.integer cimport Integer
 from sage.rings.rational cimport Rational
+from cpython.int cimport PyInt_FromLong
 
 
 def something_else(
@@ -22,25 +24,20 @@ def something_else(
     cdef fmpq* all_isog_j = calloc(ell^3 + ell^2 + ell + 1, sizeof(fmpq))
     cdef fmpq* cj = calloc(3, sizeof(fmpq))
     cdef slong cell = slong(ell)
-    for i in range(ell^3 + ell^2 + ell + 1):
-        fmpq_init(all_isog_j[i])
-    fmpq_init(cj)
     for i in range(3):
+        fmpq_init(cj + i)
         fmpq_set_mpq(cj + i, j[i].value)
 
     sig_on()
     siegel_modeq_isog_invariants_Q(nb_roots, all_isog_j, j, cell)
     sig_off()
 
-	# convert all_isog_j to list of rationals:
-	nb_roots_python = nb_roots.value
-	ans = [Rational(0) for i in range(3*nb_roots_python)]
-	for i in range(3*nb_roots_python):
-		fmpt_get_mpq((<Rational>ans[i]).value, (*all_isog_j)[i])
-		ans[i] = read
-	for i in range(ell^3 + ell^2  +ell + 1):
-        fmpq_clear(all_isog_j[i])
-	fmpq_clear(cj)
+    # convert all_isog_j to list of rationals:
+    cdef object nb_roots_python
+    nb_roots_python = PyInt_FromLong(nb_roots)
+    ans = [Rational(0) for _ in range(3*nb_roots_python)]
+    for i in range(3 * nb_roots_python):
+        fmpq_get_mpq((<Rational>ans[i]).value, *(all_isog_j + i))
     return ans
 
 
