ad076e00634bce591bd3bbc1972b4efa9dc7322e
Edgar Costa <edgarc@mit.edu>
2022-05-11 15:53:40 -0400

    some obvious fixes

--- pyhdme/hdme.pyx
+++ pyhdme/hdme.pyx
@@ -6,42 +6,43 @@
 
 from cysignals.signals cimport sig_on, sig_off
 from sage.libs.flint.types cimport fmpq
+from sage.rings.integer cimport Integer
+from sage.rings.rational cimport Rational
 
 
 def something_else(
     j,
     ell
 ):
-	cdef slong* nb_roots = calloc(sizeof(slong))
-	cdef fmpq* all_isog_j = calloc((ell^3 + ell^2 + ell + 1)*sizeof(fmpq))
-	cdef fmpq* cj = calloc(sizeof(fmpq))
-	cdef slong cell = slong(ell)
-	for i in [0..ell^3+ell^2+ell]:
-	    fmpq_init(all_isog_j[i])
-	fmpq_init(cj)
-	fmpq_set_mpq(cj, j.value)
-	
-	sig_on()
-	siegel_modeq_isog_invariants_Q(nb_roots,
-	                               all_isog_j,
-	                               j,
-	                               cell)
-	sig_off()
-	
+    cdef slong* nb_roots = calloc(sizeof(slong))
+    cdef fmpq* all_isog_j = calloc((ell^3 + ell^2 + ell + 1)*sizeof(fmpq))
+    cdef fmpq* cj = calloc(sizeof(fmpq))
+    cdef slong cell = slong(ell)
+    for i in range(ell^3 + ell^2 + ell + 1):
+        fmpq_init(all_isog_j[i])
+    fmpq_init(cj)
+    fmpq_set_mpq(cj, j.value)
+
+    sig_on()
+    siegel_modeq_isog_invariants_Q(nb_roots,
+                                   all_isog_j,
+                                   j,
+                                   cell)
+    sig_off()
+
 	# convert all_isog_j to list of rationals:
 	cdef Rational read = <Rational>Rational.__new__(Rational)
 	cdef Integer nb_roots_python = Integer(nb_roots)
-	ans = [Integer(0) for i in [1..3*nb_roots_python-1]]
-	for i in [0..3*nb_roots_python-1]:
+	ans = [Rational(0) for i in range(3*nb_roots_python)]
+	for i in range(3*nb_roots_python):
 		fmpt_get_mpq(read.value, (*all_isog_j)[i])
 		ans[i] = read
-	for i in [0..ell^3+ell^2+ell]:
-	    fmpq_clear(all_isog_j[i])
+	for i in range(ell^3 + ell^2  +ell + 1):
+        fmpq_clear(all_isog_j[i])
 	fmpq_clear(cj)
 	cdef free(nb_roots)
 	cdef free(all_isog_j)
 	cdef free(cj)
-	
     return ans
 
 
