744b92c0f8449b770f1f1743e709e0732c888226
Jean <jean.kieffer@math.cnrs.fr>
2022-06-07 11:19:55 -0400

    Write hecke_charpoly_wrapper

--- pyhdme/hdme.pyx
+++ pyhdme/hdme.pyx
@@ -4,133 +4,76 @@
 # See LICENSE file for license details.
 
 
-from sage.libs.flint.types cimport fmpq, slong
+from sage.libs.flint.types cimport fmpz_poly_t, slong
 from cysignals.signals cimport sig_on, sig_off
-from memory_allocator cimport MemoryAllocator
-from sage.libs.flint.fmpq cimport fmpq_init, fmpq_clear, fmpq_get_mpq, fmpq_set_mpq
+from sage.libs.flint.fmpz cimport fmpz_init, fmpz_clear, fmpz_get_mpz
+from sage.libs.flint.fmpz_poly cimport fmpz_poly_init, fmpz_poly_clear, fmpz_poly_get_coeff_fmpz
 from sage.rings.integer cimport Integer
 from sage.rings.rational cimport Rational
 from cpython.int cimport PyInt_FromLong
 from sage.libs.gmp.mpz cimport mpz_get_si
 from sage.all import QQ, ZZ
 
-
-
-def igusa_clebsch_to_hdme_absolute_invariants(I2, I4, I6, I10):
-    I6prime = (I2*I4-3*I6)/2
-    return (I4*I6prime/I10, I2*I4*I4/I10, I4**5/I10**2)
-
-
-def hdme_absolute_invariants_to_igusa_clebsch(j1, j2, j3):
-    # M = Matrix([[0,1,1,-1],[1,2,0,-1],[0,5,0,-2]])
-    # M.solve_left(Matrix([[-2,1,0,0],[-3,0,1,0],[-5,0,0,1]]))
-    # [ 0 -2  1]
-    # [ 1 -3  1]
-    # [ 0 -5  2]
-    I2, I4, I6prime, I10 = (1, j3/j2**2, j1*j3/j2**3, j3**2/j2**5)
-    I6 = (I2 * I4 - 2*I6prime)/3
-    return (I2, I4, I6, I10)
-
-
-def generic_wrapper(
-    steps,
-    absolute_invariants,
+def hecke_charpoly_wrapper(
     ell,
-    verbose=False
+    wt,
+    X
 ):
-    # cdef int cverbose = int(verbose)
-    # set_modeq_verbose(cverbose);
-    assert steps in [1,2]
-    assert len(absolute_invariants) == 3
-    j = [QQ(elt) for elt in absolute_invariants]
+    r"""
+    Given a prime number ell and an even weight wt >= 4, compute the characteristic polynomial of the Hecke operator of level ell on the space of Siegel modular forms of degree 2, full level, and weight wt, as a polynomial in X
+
+    INPUT:
+
+    - ``ell`` -- a prime number
+    - ``wt`` -- an even number greater than 3
+    - ``X`` -- a variable in a polynomial ring
+    
+
+    OUTPUT:
+
+    - The characteristic polynomial of the Hecke operator, as a polynomial in X
+
+    Examples::
+
+
+    """
+    
     ell = ZZ(ell)
+    wt = ZZ(wt)
     assert ell.is_prime()
-    cdef MemoryAllocator mem = MemoryAllocator()
-    cdef slong nb_roots;
-    max_nb_roots = (ell**3 + ell**2 + ell + 1)**steps
-    cdef fmpq* all_isog_j = <fmpq*>mem.calloc(3 * max_nb_roots, sizeof(fmpq))
-    cdef fmpq* cj = <fmpq*>mem.calloc(3, sizeof(fmpq))
+    
+    cdef fmpz_poly_t charpoly
+    cdef fmpz_t coeff
+    cdef slong d
+    cdef slong j
+    
     cdef slong cell = mpz_get_si((<Integer>ell).value)
-    for i in range(3):
-        fmpq_init(&cj[i])
-        fmpq_set_mpq(&cj[i], (<Rational>j[i]).value)
-    for i in range(3*max_nb_roots):
-        fmpq_init(&all_isog_j[i]);
+    cdef slong cwt = mpz_get_si((<Integer>wt).value)
+    
+    fmpz_poly_init(charpoly)
+    fmpz_init(coeff)
 
     sig_on()
-    if steps == 1:
-        assert siegel_modeq_isog_invariants_Q(&nb_roots, all_isog_j, cj, cell) == 1
-    elif steps == 2:
-        assert siegel_modeq_2step_isog_invariants_Q(&nb_roots, all_isog_j, cj, cell) == 1
+    assert hecke_charpoly(charpoly, cell, cwt) == 1
     sig_off()
 
-    # convert all_isog_j to list of rationals:
-    cdef object nb_roots_python
-    nb_roots_python = PyInt_FromLong(nb_roots)
-    assert nb_roots_python <= max_nb_roots;
-    ans = [Rational(0) for _ in range(3*nb_roots_python)]
-    for i in range(3 * nb_roots_python):
-        fmpq_get_mpq((<Rational>ans[i]).value, &all_isog_j[i])
+    d = fmpz_poly_degree(charpoly)
 
-    for i in range(3):
-        fmpq_clear(&cj[i])
-    for i in range(3*max_nb_roots):
-        fmpq_clear(&all_isog_j[i]);
-    res = [(ans[3*i], ans[3*i + 1], ans[3*i + 2]) for i in range(nb_roots_python)]
-    return res
+    # convert charpoly to polynomial
+    cdef object d_python
+    d_python = PyInt_FromLong(d)
+    
+    ans = Rational(0) * X
+    c = ZZ(0)
 
+    for i in range(d_python+1):
+        j = mpz_get_si((<Integer> ZZ(i)).value)
+        fmpz_poly_get_coeff_fmpz(coeff, charpoly, j)
+        fmpz_get_mpz((<Integer> c).value, coeff)
+        ans += c * X**i
 
-def siegel_modeq_2step_isog_invariants_Q_wrapper(
-    igusa_clebsch_invariants,
-    ell,
-    verbose=False
-):
-    r"""
-    Given the Igusa-Clebsch invariants `I_2, I_4, I_6, I_{10}` of Igusa and Clebsch [IJ1960]_
-    of an abelian surface compute the Igusa-Clebsch invariants of surfaces (ell,ell,ell^2)-isogenous
+    fmpz_poly_clear(charpoly)
+    fmpz_clear(coeff)
+    return ans
 
-    INPUT:
-
-    - ``igusa_clebsch_invariants`` -- the Igusa-Clebsch invariants `I_2, I_4, I_6, I_{10}` of Igusa and Clebsch [IJ1960]_
-    - ``ell`` -- a prime number
-
-    OUTPUT:
-
-    - A list of Igusa-Clebsch invariants of abelian surfaces (ell, ell)-isogenous
-
-    Examples::
-
-
-    """
-    I2, I4, I6, I10 = igusa_clebsch_invariants
-    absolute_invariants = igusa_clebsch_to_hdme_absolute_invariants(I2, I4, I6, I10)
-    res = generic_wrapper(1, absolute_invariants, ell, verbose=verbose)
-    return [hdme_absolute_invariants_to_igusa_clebsch(*elt) for elt in res]
-
-def siegel_modeq_isog_invariants_Q_wrapper(
-    igusa_clebsch_invariants,
-    ell,
-    verbose=False
-):
-    r"""
-    Given the Igusa-Clebsch invariants `I_2, I_4, I_6, I_{10}` of Igusa and Clebsch [IJ1960]_
-    of an abelian surface compute the Igusa-Clebsch invariants of surfaces (ell,ell)-isogenous
-
-    INPUT:
-
-    - ``igusa_clebsch_invariants`` -- the Igusa-Clebsch invariants `I_2, I_4, I_6, I_{10}` of Igusa and Clebsch [IJ1960]_
-    - ``ell`` -- a prime number
-
-    OUTPUT:
-
-    - A list of Igusa-Clebsch invariants of abelian surfaces (ell, ell)-isogenous
-
-    Examples::
-
-
-    """
-    I2, I4, I6, I10 = igusa_clebsch_invariants
-    absolute_invariants = igusa_clebsch_to_hdme_absolute_invariants(I2, I4, I6, I10)
-    res = generic_wrapper(1, absolute_invariants, ell, verbose=verbose)
-    return [hdme_absolute_invariants_to_igusa_clebsch(*elt) for elt in res]
 
