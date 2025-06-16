89c728f68df90b7eb65a7a001095437ee0544c34
Jean Kieffer <jean.kieffer@math.cnrs.fr>
2025-06-16 16:57:39 +0200

    Attempt at class for rigidified PPAS

---
+++ pyhdme/g2_curve_invariants.py
@@ -0,0 +1,23 @@
+
+# We call I the following vector of covariants:
+# psi4 = I4/4
+# psi6 = I6prime/4 (Streng's notation)
+# chi10 = -I10/2^12
+# chi12 = I12/2^15
+
+# They correspond to the Siegel modular forms with the following
+# normalized q-expansions:
+# psi4 = 1 + 240(q1+q2) + ...
+# psi6 = 1 - 504(q1+q1) + ...
+# chi10 = (q3 - 2 + q3^-1) + ...
+# chi12 = (q3 + 10 + q3^-1) + ...
+
+# This normalization differs slightly from Igusa's, who divides
+# further chi10 and chi12 by 4 and 12, respectively.
+
+# Other sets of covariants are:
+# - Streng I4, I6prime, I10, I12,
+# - classical Igusa--Clebsch I2, I4, I6, I10,
+# - Clebsch A, B, C, D
+# - Igusa Y12, X16, ..., X48
+
