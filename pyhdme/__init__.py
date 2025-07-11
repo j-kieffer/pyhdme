ae148d959e64a55c50ad154d010a8fa21ac1c4a2
Jean Kieffer <jean.kieffer@math.cnrs.fr>
2025-07-11 10:57:32 +0200

    Add PPASComplexBallField, rework PPASRationalField accordingly

--- pyhdme/__init__.py
+++ pyhdme/__init__.py
@@ -4,13 +4,17 @@ from .hdme import (
     siegel_modeq_isog_invariants_Q_wrapper,
     siegel_modeq_2step_isog_invariants_Q_wrapper,
 )
-from .ppas_invariants import (
-    PPASInvariants
-)
-
 assert igusa_clebsch_from_modular_igusa
 assert modular_igusa_from_igusa_clebsch
 assert siegel_modeq_isog_invariants_Q_wrapper
 assert siegel_modeq_2step_isog_invariants_Q_wrapper
 
+from .ppas_invariants import (
+    PPASInvariants
+)
 assert PPASInvariants
+
+from .ppas_complex_ball_field import (
+    PPASComplexBallField
+)
+assert PPASComplexBallField
--- pyhdme/ppas_rational_field.py
+++ pyhdme/ppas_rational_field.py
@@ -9,6 +9,7 @@ AUTHORS:
 
 from sage.rings.complex_arb import ComplexBallField
 from pyhdme.ppas_invariants import PPASInvariants
+from pyhdme.ppax_complex_ball_field import PPASComplexBallField
 
 # Copyright 2025 Jean Kieffer
 # See LICENSE file for license details.
@@ -34,25 +35,34 @@ class PPASRationalField(SageObject):
         self.__qq_invariants = PPASInvariants.__init__(data, inv_type)
         self.__cplx_prec = prec
         self.__cplx_invariants = self.__qq_invariants.change_ring(CC, force_exact_computations = True)
-        self.__cplx_roots_prec = 0
-        self.__cplx_roots = None
-        self.__cplx_periods_prec = 0
-        self.__cplx_periods = None
+        self.__cplx_ppas = PPASComplexBallField(self.__cplx_invariants, prec = self.__cplx_prec)
 
     def rational_invariants(self):
         return self.__qq_invariants
 
-    def complex_invariants(self, prec = self.__cplx_prec):
+    def increase_prec(self, prec):
         if self.__cplx_prec < prec:
             self.__cplx_prec = prec
             # this includes base-changing the curve equation if it was already computed.
             self.__cplx_invariants = self.rational_invariants().change_ring(CC)
-            return self.__cplx_invariants
+            # this keeps thomae signs, etc. if they were already computed.
+            self.__cplx_ppas = PPASComplexBallField(self.__cplx_invariants, prec = self.__cplx_prec,
+                                                    low_precision = self.__cplx_ppas)
+    def complex_invariants(self, prec = self.__cplx_prec):
+        if self.__cplx_prec < prec:
+            self.increase_prec(prec)
         elif self.__cplx_prec > prec:
             CC = ComplexBallField(prec)
             return self.__cplx_invariants.change_ring(CC)
-        else:
-            return self.__cplx_invariants
+        return self.__cplx_invariant
+
+    def complex_ppas(self, prec = self.__cplx_prec):
+        if self.__cplx_prec < prec:
+            self.increase_prec(prec)
+        elif self.__cplx_prec > prec:
+            CC = ComplexBallField(prec)
+            return self.__cplx_ppas.change_ring(CC)
+        return self.__cplx_ppas
 
     def is_geometrically_split(self):
         return self.rational_invariants().is_geometrically_split()
@@ -66,72 +76,30 @@ class PPASRationalField(SageObject):
         return self.complex_invariants(prec).genus_2_curve_equation()
 
     def complex_weierstrass_points(self, prec = self.__cplx_prec):
-        if self.__cplx_roots is None:
-            p = max(prec, 32)
-            done = False
-            while not done:
-                self.__cplx_roots_prec = p
-                try:
-                    rts = Sequence(self.complex_genus_2_curve(p).roots(multiplicities = False),
-                                   universe = self.complex_field())
-                except ValueError: #unable to isolate roots
-                    #todo: this will happen if the complex curve is a degree 5 model...
-                    continue
-                if len(rts) < 6:
-                    continue
-                done = True
-                for i in range(7):
-                    for j in range(i + 1, 7):
-                        if rts[i].overlaps(rts[j]):
-                            done = False
+        p = max(prec, 32)
+        while True:
+            try:
+                return self.complex_ppas(p).weierstrass_points()
+            except ValueError:
                 p *= 2
-            self.__cplx_roots = rts
-            return self.__cplx_roots
-        elif self.__cplx_roots_prec < prec:
-            old_rts = self.__cplx_roots
-            new_crv = self.complex_genus_2_curve(prec) #increases complex precision
-            CC = self.complex_field()
-            new_rts = Sequence(new_crv.roots(multiplicities = False), universe = CC)
-            self.__cplx_roots = [0 for i in range(7)]
-            for i in range(7):
-                nb = 0
-                for j in range(7):
-                    if new_rts[i].overlaps(old_rts[j]):
-                        nb++
-                        self.__cplx_roots[j] = new_rts[i]
-                if nb != 1:
-                    raise ValueError("Zero or several overlaps between Weierstrass points when increasing precision from {} to {}".format(self.__cplx_roots_prec, prec))
-            self.__cplx_roots_prec = prec
-            self.__cplx_roots = Sequence(self.__cplx_roots, universe = CC)
-            return self.__cplx_roots
-        else:
-            CC = ComplexBallField(prec)
-            return Sequence(self.__cplx_roots, universe = CC)
 
-    @cached
-    def thomae_signs(self):
-        #call C library. Return a permutation of the roots and a set of sign choices
+    def reduced_small_period_matrix(self, prec = self.__cplx_prec):
+        p = max(prec, 32)
+        while True:
+            try:
+                return self.complex_ppas(p).reduced_small_period_matrix()
+            except ValueError:
+                p *= 2
 
-    def rosenhain_invariants(self, prec):
-        rts = self.complex_weierstrass_points(prec)
-        #call C library with perm
+    def rosenhain_invariants(self, prec = self.__cplx_prec):
+        self.reduced_small_period_matrix()
+        return self.complex_ppas().rosenhain_invariants()
 
-    def theta4(self, prec):
-        if self.is_geometrically_split():
-            pass
-        else:
-            ros = self.rosenhain_invariants(prec);
-            #call C library
+    def theta2(self, prec = self.__cplx_prec):
+        self.reduced_small_period_matrix()
+        return self.complex_ppas().theta2()
 
-    def theta2(self, prec):
-        if self.is_geometrically_split():
-            pass
-        else:
-            ros = self.rosenhain_invariants(prec)
-            th4 = self.theta4(prec)
-            #call C library with signs
-
-    def complex_j_invariants(self, prec):
+    def complex_j_invariants(self, prec = self.__cplx_prec):
         CC = ComplexBallField(prec)
         pol = self.rational_invariants().j_invariants_quadratic_equation()
         delta = pol.discriminant()
@@ -141,19 +109,4 @@ class PPASRationalField(SageObject):
         else:
             j = pol.roots(ComplexBallField(prec), multiplicities = False)
             return Sequence(j, universe = CC)
-
-    def reduced_small_period_matrix(self, prec):
-        if self.__cplx_periods is None or self.__cplx_periods_prec < prec:
-            if self.is_geometrically_split():
-                j1, j2 = self.complex_j_invariants(prec)
-                tau1 = #call C library
-                tau2 = #call C library
-                CC = j1.parent()
-                self.__cplx_periods_prec = prec
-                self.__cplx_periods = Matrix(CC, [[tau1, 0], [0, tau2]])
-            else:
-                th2 = self.theta2(prec)
-                #call C library
-
-    
 
